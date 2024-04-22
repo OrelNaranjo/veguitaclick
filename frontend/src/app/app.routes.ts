b4dd6c9b39277486467d2fe5cdbbf6cd251a3c1d
Orel Naranjo <or.naranjo@duocuc.cl>
2024-04-21 23:27:38 -0400

    Agregar rutas de productos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './pages/product/product-list/product-list.component';
+import { ProductComponent } from './pages/product/product/product.component';
+import { ProductAddComponent } from './pages/product/product-add/product-add.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Rutas de productos
+    { path: 'productos', component: ProductListComponent },
+    { path: 'producto/:id', component: ProductComponent },
+    { path: 'producto/add', component: ProductAddComponent},
+    { path: 'producto/edit/:id', component: ProductAddComponent},
+
+    // Ruta por defecto
+    { path: '', redirectTo: '/', pathMatch: 'full' },
+    { path: '**', redirectTo: '/' }
+
+];
