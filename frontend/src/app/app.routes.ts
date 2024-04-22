6e4366395b99a826fac4a13daeabb40a5d173266
Orel Naranjo <or.naranjo@duocuc.cl>
2024-04-22 14:24:36 -0400

    Agregar cambion en rutas y agregar pagina home y error404

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,17 +1,23 @@
 import { Routes } from '@angular/router';
+
+import { HomeComponent } from './pages/system/home/home.component';
 import { ProductListComponent } from './pages/product/product-list/product-list.component';
 import { ProductComponent } from './pages/product/product/product.component';
 import { ProductAddComponent } from './pages/product/product-add/product-add.component';
+import { Page404Component } from './pages/system/page404/page404.component';
+
 
 export const routes: Routes = [
     // Rutas de productos
-    { path: 'productos', component: ProductListComponent },
-    { path: 'producto/:id', component: ProductComponent },
-    { path: 'producto/add', component: ProductAddComponent},
+    { path: 'home', component: HomeComponent },
+    { path: '404', component: Page404Component},
+    { path: 'products', component: ProductListComponent },
+    { path: 'product/new', component: ProductAddComponent},
     { path: 'producto/edit/:id', component: ProductAddComponent},
+    { path: 'product/:id', component: ProductComponent },
 
     // Ruta por defecto
-    { path: '', redirectTo: '/', pathMatch: 'full' },
-    { path: '**', redirectTo: '/' }
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: '404' }
 
 ];
