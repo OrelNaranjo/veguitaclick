a7f7371a43ac6453c9e914ae708a97d8fe057147
Orel Naranjo <or.naranjo@duocuc.cl>
2024-04-22 14:31:24 -0400

    Agregar los estados del titulo y direcciones de la pagina

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { StateService } from '../../services/state.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
+  providers: [StateService],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  title!: string;
 
+  constructor(private router: Router, public stateService: StateService) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.stateService.updateTitleAndBreadcrumb(event.url);
+      }
+    });
+  }
 }
