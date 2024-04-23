d837977dcc436e2a7793166fbff9e100422c78b6
Orel Naranjo <or.naranjo@duocuc.cl>
2024-04-23 11:23:41 -0400

    Agregado cololor rojo a las celdas de productos descontinuados

--- frontend/src/app/pages/product/product-list/product-list.component.ts
+++ frontend/src/app/pages/product/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
@@ -16,70 +17,80 @@ export class ProductListComponent {
       sku: 'PRO1',
       name: 'Product 1',
       description: 'Description 1',
-      price: 1000
+      price: 1000,
+      discontinued: true
     },
     {
       id: 2,
       sku: 'PRO2',
       name: 'Product 2',
       description: 'Description 2',
-      price: 2000
+      price: 2000,
+      discontinued: false
     },
     {
       id: 3,
       sku: 'PRO3',
       name: 'Product 3',
       description: 'Description 3',
-      price: 3000
+      price: 3000,
+      discontinued: true
     },
     {
       id: 4,
       sku: 'PRO4',
       name: 'Product 4',
       description: 'Description 4',
-      price: 4000
+      price: 4000,
+      discontinued: false
     },
     {
       id: 5,
       sku: 'PRO5',
       name: 'Product 5',
       description: 'Description 5',
-      price: 5000
+      price: 5000,
+      discontinued: false
     },
     {
       id: 6,
       sku: 'PRO6',
       name: 'Product 6',
       description: 'Description 6',
-      price: 6000
+      price: 6000,
+      discontinued: false
     },
     {
       id: 7,
       sku: 'PRO7',
       name: 'Product 7',
       description: 'Description 7',
-      price: 7000
+      price: 7000,
+      discontinued: false
     },
     {
       id: 8,
       sku: 'PRO8',
       name: 'Product 8',
       description: 'Description 8',
-      price: 8000
+      price: 8000,
+      discontinued: true
     },
     {
       id: 9,
       sku: 'PRO9',
       name: 'Product 9',
       description: 'Description 9',
-      price: 9000
+      price: 9000,
+      discontinued: false
     },
     {
       id: 10,
       sku: 'PRO10',
       name: 'Product 10',
       description: 'Description 10',
-      price: 10000
+      price: 10000,
+      discontinued: false
     }
   ];
 
