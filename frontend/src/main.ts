cf835f8cf3f4898807d39cd5627037a42fed53c2
Orel Naranjo <or.naranjo@duocuc.cl>
2024-04-23 15:42:46 -0400

    Agregar Localizacion CL

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { ContentComponent } from './app/components/content/content.component';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es-CL';
 
 bootstrapApplication(ContentComponent, appConfig)
   .catch((err) => console.error(err));
+
+registerLocaleData(localeEs, 'cl');
