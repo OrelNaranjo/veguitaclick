ef3c364207b4f83774ef050582a5afe88f23e8b0
damonjaguer <roromontoya1999@gmail.com>
2024-04-27 23:51:07 -0400

    agregado servicio proveedor

---
+++ frontend/src/app/services/supplier.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { API_BASE_URL } from '../config/config';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupplierService {
+
+  private supplierUrl= `${API_BASE_URL}/suppliers`;
+
+  constructor(private http: HttpClient) { }
+  getAllSuppliers() {
+    return this.http.get(this.supplierUrl);
+  }
+
+  getSupplierById(id: number) {
+    return this.http.get(`${this.supplierUrl}/${id}`);
+  }
+
+  createSupplier(supplier: any) {
+    return this.http.post(this.supplierUrl, supplier);
+  }
+
+  updateSupplier(id: number, supplier: any) {
+    return this.http.put(`${this.supplierUrl}/${id}`, supplier);
+  }
+
+  deleteSupplier(id: number) {
+    return this.http.delete(`${this.supplierUrl}/${id}`);
+  }
+
+}
