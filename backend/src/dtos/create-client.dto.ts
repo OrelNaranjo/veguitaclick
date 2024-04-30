a3997ba6359512ea5142bb773cc3e61864aa501f
Classic-Underground <marcos.vera.barrera88@gmail.com>
2024-04-30 01:39:15 -0400

    Agregado Dtos Cliente

---
+++ backend/src/dtos/create-client.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsEmail, IsPhoneNumber } from 'class-validator';
+
+export class CreateClientDto {
+    @ApiProperty({example: '11.111.111-1'})
+    @IsNotEmpty()
+    @IsString()
+    readonly rut: string;
+    
+
+    @ApiProperty({example: 'cliente'})
+    @IsNotEmpty()
+    @IsString()
+    readonly name: string;
+
+    @ApiProperty({example: 'direccion1'})
+    @IsNotEmpty()
+    @IsString()
+    readonly address: string;
+
+    @ApiProperty({example: 'cliente@cliente.cl'})
+    @IsNotEmpty()
+    @IsEmail()
+    readonly email: string;
+
+    @ApiProperty({example: '123456789'})
+    @IsNotEmpty()
+    @IsPhoneNumber('CL')
+    readonly phone: string;
+}
