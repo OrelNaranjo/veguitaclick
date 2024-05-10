cfaf744ec90abb7d176074a7a7b8d93a195a9dd1
Orel Naranjo <or.naranjo@duocuc.cl>
2024-05-09 23:14:45 -0400

    Crear DTOs y Entidad de vehiculos

---
+++ backend/src/entities/vehicle.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Vehicle {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    make: string;
+
+    @Column()
+    model: string;
+
+    @Column()
+    year: number;
+
+    @Column()
+    color: string;
+
+    @Column()
+    licensePlate: string;
+}
