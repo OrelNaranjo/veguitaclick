ff53879d4e1428baadfd51a1f77d5737091e79d5
Classic-Underground <marcos.vera.barrera88@gmail.com>
2024-05-17 18:12:49 -0400

    Creacion de módulo procesos

--- backend/src/modules/processes.module.ts
+++ backend/src/modules/processes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProcessesService } from '../services/processes/processes.service';
 import { ProcessesController } from '../controllers/processes/processes.controller';
+import { Process } from '../entities/process.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Process])],
   controllers: [ProcessesController],
   providers: [ProcessesService],
 })
